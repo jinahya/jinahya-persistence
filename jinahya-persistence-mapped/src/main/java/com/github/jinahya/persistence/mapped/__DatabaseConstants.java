14779140024c3fb4c61d02683e5ee479ac283625
Jin Kwon <onacit@gmail.com>
2025-06-26 18:19:06 +0900

    wip

--- jinahya-persistence-mapped/src/main/java/com/github/jinahya/persistence/mapped/__DatabaseConstants.java
+++ jinahya-persistence-mapped/src/main/java/com/github/jinahya/persistence/mapped/__DatabaseConstants.java
@@ -2,14 +2,26 @@ package com.github.jinahya.persistence.mapped;
 
 public abstract class __DatabaseConstants {
 
+    // ----------------------------------------------------------------------------------------------------------- MySQL
     public abstract class __MySQL {
 
+        public static final long MIN_INT = Integer.MIN_VALUE;
+
+        public static final long MAX_INT = Integer.MAX_VALUE;
+
+        public static final long MIN_INT_UNSIGNED = 0L;
+
+        public static final long MAX_INT_UNSIGNED = 4_294_967_295L;
+
+        // ------------------------------------------------------------------------------------------------ CONSTRUCTORS
         protected __MySQL() {
             super();
         }
     }
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------------ PostgreSQL
+
+    // ---------------------------------------------------------------------------------------------------- CONSTRUCTORS
     protected __DatabaseConstants() {
         super();
     }
