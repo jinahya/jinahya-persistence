cbded720843be817affcf6b3cff7b986e6cecb0e
Jin Kwon <onacit@gmail.com>
2025-06-30 15:22:25 +0900

    wip

--- jinahya-persistence-mapped-test/src/main/java/com/github/jinahya/persistence/mapped/___RandomizerUtils.java
+++ jinahya-persistence-mapped-test/src/main/java/com/github/jinahya/persistence/mapped/___RandomizerUtils.java
@@ -34,7 +34,7 @@ public final class ___RandomizerUtils {
     }
 
     // -----------------------------------------------------------------------------------------------------------------
-    ___RandomizerUtils() {
-        super();
+    private ___RandomizerUtils() {
+        throw new AssertionError("instantiation is not allowed");
     }
 }
