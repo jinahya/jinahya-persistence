b9d1b6b157e129b3c87fcda42ccea348e6471f05
Jin Kwon <onacit@gmail.com>
2025-07-07 17:23:40 +0900

    wip

---
+++ jinahya-persistence-more/src/main/java/com/github/jinahya/persistence/more/__AttributeEnum.java
@@ -0,0 +1,5 @@
+package com.github.jinahya.persistence.more;
+
+public interface __AttributeEnum<E extends Enum<E> & __AttributeEnum<E>> {
+
+}
---
+++ jinahya-persistence-more/src/test/java/com/github/jinahya/persistence/more/__AttributeEnumTest.java
@@ -0,0 +1,7 @@
+package com.github.jinahya.persistence.more;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class __AttributeEnumTest {
+
+}
