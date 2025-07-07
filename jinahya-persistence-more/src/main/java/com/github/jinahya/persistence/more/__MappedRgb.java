ee70f1886125733ec6c3b1f990ebc7ddf9deac7f
Jin Kwon <onacit@gmail.com>
2025-07-07 11:04:14 +0900

    wip

--- jinahya-persistence-more/src/main/java/com/github/jinahya/persistence/more/__MappedRgb.java
+++ jinahya-persistence-more/src/main/java/com/github/jinahya/persistence/more/__MappedRgb.java
@@ -5,13 +5,10 @@ import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Transient;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 
 import java.io.Serial;
 import java.util.Objects;
 import java.util.function.DoubleFunction;
-import java.util.function.IntFunction;
 
 /**
  * .
@@ -22,14 +19,34 @@ import java.util.function.IntFunction;
 @MappedSuperclass
 @SuppressWarnings({
         "java:S100", // Method names should comply with a naming convention
+        "java:S101", // Class names should comply with a naming convention
         "java:S115", // Constant names should comply with a naming convention
         "java:S116", // Field names should comply with a naming convention
-        "java:S117"  // Local variable and method parameter names should comply with a naming convention
+        "java:S117",  // Local variable and method parameter names should comply with a naming convention
+        "java:S119" // Type parameter names should comply with a naming convention
 })
 public abstract class __MappedRgb<SELF extends __MappedRgb<SELF>> extends ___MappedColor<SELF> {
 
     @Serial
     private static final long serialVersionUID = -5905278228337798453L;
+
+    // -----------------------------------------------------------------------------------------------------------------
+    public static final String DECIMAL_MIN_COMPONENT = "0.0";
+
+    public static final String DECIMAL_MAX_COMPONENT = "255.0";
+
+    public static final float MIN_COMPONENT = 0.0f;
+
+    public static final float MAX_COMPONENT = 255.0f;
+
+    // -----------------------------------------------------------------------------------------------------------------
+    public static final String DECIMAL_MIN_NORMALIZED_COMPONENT = "0.0";
+
+    public static final String DECIMAL_MAX_NORMALIZED_COMPONENT = "1.0";
+
+    public static final float MIN_NORMALIZED_COMPONENT = 0.0f;
+
+    public static final float MAX_NORMALIZED_COMPONENT = 1.0f;
 
     // -----------------------------------------------------------------------------------------------------------------
     protected static final int COMPONENT_INDEX_R = 0;
@@ -67,42 +84,31 @@ public abstract class __MappedRgb<SELF extends __MappedRgb<SELF>> extends ___Map
 
     @Nonnull
     public String toHexadecimalNotation3() {
-        return String.format("%1$x%2$x%3$x", getRed() >> 4, getGreen() >> 4, getBlue() >> 4);
+        return String.format("%1$x%2$x%3$x", ((int) getRed()) >> 4, ((int) getGreen()) >> 4, ((int) getBlue()) >> 4);
     }
 
     @Nonnull
     public String toHexadecimalNotation4() {
-        return String.format("%1$s%2$x", toHexadecimalNotation3(), getAlpha() >> 4);
+        return String.format("%1$s%2$x", toHexadecimalNotation3(), ((int) getAlpha()) >> 4);
     }
 
     @Nonnull
     public String toHexadecimalNotation6() {
-        return String.format("%1$02x%2$02x%3$02x", getRed(), getGreen(), getBlue());
+        return String.format("%1$02x%2$02x%3$02x", (int) getRed(), (int) getGreen(), (int) getBlue());
     }
 
     @Nonnull
     public String toHexadecimalNotation8() {
-        return String.format("%1$s%2$02x", toHexadecimalNotation6(), getAlpha());
+        return String.format("%1$s%2$02x", toHexadecimalNotation6(), (int) getAlpha());
     }
 
     // -----------------------------------------------------------------------------------------------------------------
 
-    /**
-     * Applies all components to the specified function, and returns the result.
-     * <p>
-     * {@snippet lang = "java":
-     * final var color = entity.apply(r -> g -> b -> a -> new java.awt.Color(r, g, b, a));
-     *}
-     *
-     * @param function the function
-     * @param <R>      result type parameter
-     * @return the result of the {@code function}.
-     */
     public <R> R applyComponents(
-            final IntFunction< // r
-                    ? extends IntFunction< // g
-                            ? extends IntFunction< // b
-                                    ? extends IntFunction< // a
+            final DoubleFunction< // r
+                    ? extends DoubleFunction< // g
+                            ? extends DoubleFunction< // b
+                                    ? extends DoubleFunction< // a
                                             ? extends R>>>> function) {
         return Objects.requireNonNull(function, "function is null")
                 .apply(getRed())
@@ -125,179 +131,161 @@ public abstract class __MappedRgb<SELF extends __MappedRgb<SELF>> extends ___Map
     }
 
     // ------------------------------------------------------------------------------------------------------------- red
-    @Max(___MappedColorConstants.RGB_MAX_COMPONENT)
-    @Min(___MappedColorConstants.RGB_MIN_COMPONENT)
+    @DecimalMax(DECIMAL_MAX_COMPONENT)
+    @DecimalMin(DECIMAL_MIN_COMPONENT)
     @Transient
-    public int getRed() {
-        return (int) getComponent(COMPONENT_INDEX_R);
+    public float getRed() {
+        return getComponent(COMPONENT_INDEX_R);
     }
 
-    public void setRed(final int red) {
-        if (red < ___MappedColorConstants.RGB_MIN_COMPONENT ||
-            red > ___MappedColorConstants.RGB_MAX_COMPONENT) {
+    public void setRed(final float red) {
+        if (red < MIN_COMPONENT || red > MAX_COMPONENT) {
             throw new IllegalArgumentException("invalid red: " + red);
         }
-        setComponent(
-                COMPONENT_INDEX_R,
-                red
-        );
+        setComponent(COMPONENT_INDEX_R, red);
     }
 
     @SuppressWarnings({"unchecked"})
-    public SELF red(final int red) {
+    public SELF red(final float red) {
         setRed(red);
         return (SELF) this;
     }
 
-    @DecimalMax(___MappedColorConstants.DECIMAL_MAX_COMPONENT_NORMALIZED)
-    @DecimalMin(___MappedColorConstants.DECIMAL_MIN_COMPONENT_NORMALIZED)
-    public double getNormalizedRed() {
-        return getRed() / (double) ___MappedColorConstants.RGB_MAX_COMPONENT;
+    @DecimalMax(DECIMAL_MAX_NORMALIZED_COMPONENT)
+    @DecimalMin(DECIMAL_MIN_NORMALIZED_COMPONENT)
+    public float getNormalizedRed() {
+        return getRed() / MAX_COMPONENT;
     }
 
-    public void setNormalizedRed(final double normalizedRed) {
-        if (normalizedRed < ___MappedColorConstants.MIN_COMPONENT_NORMALIZED ||
-            normalizedRed > ___MappedColorConstants.MAX_COMPONENT_NORMALIZED) {
+    public void setNormalizedRed(final float normalizedRed) {
+        if (normalizedRed < MIN_NORMALIZED_COMPONENT || normalizedRed > MAX_NORMALIZED_COMPONENT) {
             throw new IllegalArgumentException("invalid normalized red: " + normalizedRed);
         }
-        setRed((int) (normalizedRed * ___MappedColorConstants.RGB_MAX_COMPONENT));
+        setRed(normalizedRed * MAX_COMPONENT);
     }
 
     @SuppressWarnings({"unchecked"})
-    public SELF normalizedRed(final double normalizedRed) {
+    public SELF normalizedRed(final float normalizedRed) {
         setNormalizedRed(normalizedRed);
         return (SELF) this;
     }
 
     // ----------------------------------------------------------------------------------------------------------- green
-    @Max(___MappedColorConstants.RGB_MAX_COMPONENT)
-    @Min(___MappedColorConstants.RGB_MIN_COMPONENT)
+    @DecimalMax(DECIMAL_MAX_COMPONENT)
+    @DecimalMin(DECIMAL_MIN_COMPONENT)
     @Transient
-    public int getGreen() {
-        return (int) getComponent(COMPONENT_INDEX_G);
+    public float getGreen() {
+        return getComponent(COMPONENT_INDEX_G);
     }
 
-    public void setGreen(final int green) {
-        if (green < ___MappedColorConstants.RGB_MIN_COMPONENT ||
-            green > ___MappedColorConstants.RGB_MAX_COMPONENT) {
+    public void setGreen(final float green) {
+        if (green < MIN_COMPONENT || green > MAX_COMPONENT) {
             throw new IllegalArgumentException("invalid green: " + green);
         }
         setComponent(COMPONENT_INDEX_G, green);
     }
 
     @SuppressWarnings({"unchecked"})
-    public SELF green(final int green) {
+    public SELF green(final float green) {
         setGreen(green);
         return (SELF) this;
     }
 
-    @DecimalMax(___MappedColorConstants.DECIMAL_MAX_COMPONENT_NORMALIZED)
-    @DecimalMin(___MappedColorConstants.DECIMAL_MIN_COMPONENT_NORMALIZED)
-    public double getNormalizedGreen() {
-        return getGreen() / (double) ___MappedColorConstants.RGB_MAX_COMPONENT;
+    @DecimalMax(DECIMAL_MAX_NORMALIZED_COMPONENT)
+    @DecimalMin(DECIMAL_MIN_NORMALIZED_COMPONENT)
+    public float getNormalizedGreen() {
+        return getGreen() / MAX_COMPONENT;
     }
 
-    public void setNormalizedGreen(final double normalizedGreen) {
-        if (normalizedGreen < ___MappedColorConstants.MIN_COMPONENT_NORMALIZED ||
-            normalizedGreen > ___MappedColorConstants.MAX_COMPONENT_NORMALIZED) {
+    public void setNormalizedGreen(final float normalizedGreen) {
+        if (normalizedGreen < MIN_NORMALIZED_COMPONENT || normalizedGreen > MAX_NORMALIZED_COMPONENT) {
             throw new IllegalArgumentException("invalid normalized green: " + normalizedGreen);
         }
-        setGreen((int) (normalizedGreen * ___MappedColorConstants.RGB_MAX_COMPONENT));
+        setGreen(normalizedGreen * MAX_COMPONENT);
     }
 
     @SuppressWarnings({"unchecked"})
-    public SELF normalizedGreen(final double normalizedGreen) {
+    public SELF normalizedGreen(final float normalizedGreen) {
         setNormalizedGreen(normalizedGreen);
         return (SELF) this;
     }
 
     // ------------------------------------------------------------------------------------------------------------ blue
-    @Max(___MappedColorConstants.RGB_MAX_COMPONENT)
-    @Min(___MappedColorConstants.RGB_MIN_COMPONENT)
+    @DecimalMax(DECIMAL_MAX_COMPONENT)
+    @DecimalMin(DECIMAL_MIN_COMPONENT)
     @Transient
-    public int getBlue() {
-        return (int) getComponent(COMPONENT_INDEX_B);
+    public float getBlue() {
+        return getComponent(COMPONENT_INDEX_B);
     }
 
-    public void setBlue(final int blue) {
-        if (blue < ___MappedColorConstants.RGB_MIN_COMPONENT ||
-            blue > ___MappedColorConstants.RGB_MAX_COMPONENT) {
+    public void setBlue(final float blue) {
+        if (blue < MIN_COMPONENT || blue > MAX_COMPONENT) {
             throw new IllegalArgumentException("invalid blue: " + blue);
         }
-        setComponent(
-                COMPONENT_INDEX_B,
-                blue
-        );
+        setComponent(COMPONENT_INDEX_B, blue);
     }
 
     @SuppressWarnings({"unchecked"})
-    public SELF blue(final int blue) {
+    public SELF blue(final float blue) {
         setBlue(blue);
         return (SELF) this;
     }
 
-    @DecimalMax(___MappedColorConstants.DECIMAL_MAX_COMPONENT_NORMALIZED)
-    @DecimalMin(___MappedColorConstants.DECIMAL_MIN_COMPONENT_NORMALIZED)
-    public double getNormalizedBlue() {
-        return getBlue() / (double) ___MappedColorConstants.RGB_MAX_COMPONENT;
+    @DecimalMax(DECIMAL_MAX_NORMALIZED_COMPONENT)
+    @DecimalMin(DECIMAL_MIN_NORMALIZED_COMPONENT)
+    public float getNormalizedBlue() {
+        return getBlue() / MAX_COMPONENT;
     }
 
-    public void setNormalizedBlue(final double normalizedBlue) {
-        if (normalizedBlue < ___MappedColorConstants.MIN_COMPONENT_NORMALIZED ||
-            normalizedBlue > ___MappedColorConstants.MAX_COMPONENT_NORMALIZED) {
+    public void setNormalizedBlue(final float normalizedBlue) {
+        if (normalizedBlue < MIN_NORMALIZED_COMPONENT || normalizedBlue > MAX_NORMALIZED_COMPONENT) {
             throw new IllegalArgumentException("invalid normalized blue: " + normalizedBlue);
         }
-        setBlue(
-                (int) (normalizedBlue * ___MappedColorConstants.RGB_MAX_COMPONENT)
-        );
+        setBlue(normalizedBlue * MAX_COMPONENT);
     }
 
     @SuppressWarnings({"unchecked"})
-    public SELF normalizedBlue(final double normalizedBlue) {
+    public SELF normalizedBlue(final float normalizedBlue) {
         setNormalizedBlue(normalizedBlue);
         return (SELF) this;
     }
 
     // ----------------------------------------------------------------------------------------------------------- alpha
-    @Max(___MappedColorConstants.RGB_MAX_COMPONENT)
-    @Min(___MappedColorConstants.RGB_MIN_COMPONENT)
+    @DecimalMax(DECIMAL_MAX_COMPONENT)
+    @DecimalMin(DECIMAL_MIN_COMPONENT)
     @Transient
-    public int getAlpha() {
-        return (int) getComponent(COMPONENT_INDEX_A);
+    public float getAlpha() {
+        return getComponent(COMPONENT_INDEX_A);
     }
 
-    public void setAlpha(final int alpha) {
-        if (alpha < ___MappedColorConstants.RGB_MIN_COMPONENT ||
-            alpha > ___MappedColorConstants.RGB_MAX_COMPONENT) {
+    public void setAlpha(final float alpha) {
+        if (alpha < MIN_COMPONENT || alpha > MAX_COMPONENT) {
             throw new IllegalArgumentException("invalid alpha: " + alpha);
         }
         setComponent(COMPONENT_INDEX_A, alpha);
     }
 
     @SuppressWarnings({"unchecked"})
-    public SELF alpha(final int alpha) {
+    public SELF alpha(final float alpha) {
         setAlpha(alpha);
         return (SELF) this;
     }
 
-    @DecimalMax(___MappedColorConstants.DECIMAL_MAX_COMPONENT_NORMALIZED)
-    @DecimalMin(___MappedColorConstants.DECIMAL_MIN_COMPONENT_NORMALIZED)
-    public double getNormalizedAlpha() {
-        return getAlpha() / (double) ___MappedColorConstants.RGB_MAX_COMPONENT;
+    @DecimalMax(DECIMAL_MAX_NORMALIZED_COMPONENT)
+    @DecimalMin(DECIMAL_MIN_NORMALIZED_COMPONENT)
+    public float getNormalizedAlpha() {
+        return getAlpha() / MAX_COMPONENT;
     }
 
-    public void setNormalizedAlpha(final double normalizedAlpha) {
-        if (normalizedAlpha < ___MappedColorConstants.MIN_COMPONENT_NORMALIZED ||
-            normalizedAlpha > ___MappedColorConstants.MAX_COMPONENT_NORMALIZED) {
+    public void setNormalizedAlpha(final float normalizedAlpha) {
+        if (normalizedAlpha < MIN_NORMALIZED_COMPONENT || normalizedAlpha > MAX_NORMALIZED_COMPONENT) {
             throw new IllegalArgumentException("invalid normalized alpha: " + normalizedAlpha);
         }
-        setAlpha(
-                (int) (normalizedAlpha * ___MappedColorConstants.RGB_MAX_COMPONENT)
-        );
+        setAlpha(normalizedAlpha * MAX_COMPONENT);
     }
 
     @SuppressWarnings({"unchecked"})
-    public SELF normalizedAlpha(final double normalizedAlpha) {
+    public SELF normalizedAlpha(final float normalizedAlpha) {
         setNormalizedAlpha(normalizedAlpha);
         return (SELF) this;
     }
