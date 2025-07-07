00c25be0d8ba0181c0a9357a435271a4d43744e1
Jin Kwon <onacit@gmail.com>
2025-07-07 17:21:58 +0900

    wip

--- jinahya-persistence-more/src/test/java/com/github/jinahya/persistence/more/__HslaTest.java
+++
@@ -1,8 +0,0 @@
-package com.github.jinahya.persistence.more;
-
-class __HslaTest extends __MappedHslTest<__Hsla> {
-
-    __HslaTest() {
-        super(__Hsla.class);
-    }
-}
--- jinahya-persistence-more/src/test/java/com/github/jinahya/persistence/more/__RgbTest.java
+++
@@ -1,8 +0,0 @@
-package com.github.jinahya.persistence.more;
-
-class __RgbTest extends __MappedRgbTest<__Rgba> {
-
-    __RgbTest() {
-        super(__Rgba.class);
-    }
-}
