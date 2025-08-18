38417e503bcc9a6af8a39a54d8a35c37cd059e04
Jin Kwon <onacit@gmail.com>
2025-08-18 18:26:18 +0900

    wip

---
+++ jinahya-persistence-mapped/src/main/java/com/github/jinahya/persistence/mapped/__MappedBuilder.java
@@ -0,0 +1,88 @@
+package com.github.jinahya.persistence.mapped;
+
+import jakarta.annotation.Nonnull;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * An abstract builder class for building a specific target type.
+ *
+ * @param <SELF> self type parameter
+ * @param <T>    target type parameter
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
+@SuppressWarnings({
+        "unchecked",
+        "java:S100", // Method names should comply with a naming convention
+        "java:S101", // Class names should comply with a naming convention
+        "java:S116", // Field names should comply with a naming convention
+        "java:S117", // Local variable and method parameter names should comply with a naming convention
+        "java:S119", // Type parameter names should comply with a naming convention
+        "java:S3011" // Reflection should not be used to increase accessibility of classes, methods, or fields
+})
+public abstract class __MappedBuilder<
+        SELF extends __MappedBuilder<SELF, T>,
+        T
+        > {
+
+    // ---------------------------------------------------------------------------------------------------- CONSTRUCTORS
+
+    /**
+     * Creates a new instance with the specified entity class.
+     *
+     * @param type the type.
+     */
+    protected __MappedBuilder(@Nonnull final Class<T> type) {
+        super();
+        this.type = Objects.requireNonNull(type, "type is null");
+    }
+
+    // ------------------------------------------------------------------------------------------------ java.lang.Object
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Builds this builder into an instance of {@link T} using specified function.
+     *
+     * @param function the function to build an instance of {@link T} applied to this builder.
+     * @return an instance of {@link T} built by specified function applied to this builder.
+     */
+    @Nonnull
+    public T build(@Nonnull final Function<? super SELF, ? extends T> function) {
+        Objects.requireNonNull(function, "function is null");
+        return function.apply((SELF) this);
+    }
+
+    /**
+     * Builds this builder into an instance of {@link T}.
+     *
+     * @return an instance of {@link T}.
+     */
+    @Nonnull
+    public T build() {
+        return build(
+                b -> {
+                    try {
+                        final var constructor = type.getDeclaredConstructor(b.getClass());
+                        if (!constructor.canAccess(null)) {
+                            constructor.setAccessible(true);
+                        }
+                        return constructor.newInstance(b);
+                    } catch (final ReflectiveOperationException roe) {
+                        throw new RuntimeException(
+                                "failed to instantiate " + type + " with " + this,
+                                roe
+                        );
+                    }
+                }
+        );
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * The type to build.
+     */
+    protected final Class<T> type;
+}
